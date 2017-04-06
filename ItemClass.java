c90326ff54522d72841292688f26d648f83e64ca
Kyle-Beckley <kyle.beckley@smail.astate.edu>
2017-04-06 13:27:53 -0500

    Add files via upload

--- ItemClass.java
+++ ItemClass.java
@@ -1,7 +1,18 @@
 /*
  *
  */
-
+package scamazon
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.PreparedStatement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.postgresql.copy.CopyManager;
+import org.postgresql.core.BaseConnection;
 /**
  *
  * @author Kyle Beckley
@@ -82,5 +93,60 @@ public abstract class ItemClass {
     
     public void setStock(boolean newStock){
         Stock = newStock;
+    }
+    
+    public File_to_Database(string FileName){
+        Connection con = null;
+        PreparedStatement pst = null;
+        ResultSet rs = null;
+        FileReader fr = null;
+
+        String url = 
+        String user = 
+        String password = 
+
+        try {
+
+            con = DriverManager.getConnection(url, user, password);
+           
+            CopyManager cm = new CopyManager((BaseConnection) con);
+
+            fr = new FileReader();
+            cm.copyIn("COPY items FROM STDIN WITH DELIMITER '/n'", fr);
+            
+        } catch (SQLException | IOException ex) {
+            Logger lgr = Logger.getLogger(CopyFrom.class.getName());
+            lgr.log(Level.SEVERE, ex.getMessage(), ex);
+
+        } finally {
+
+            try {
+                if (rs != null) {
+                    rs.close();
+                }
+                if (pst != null) {
+                    pst.close();
+                }
+                if (con != null) {
+                    con.close();
+                }
+                if (fr != null) {
+                    fr.close();
+                }
+
+            } catch (SQLException | IOException ex) {
+                Logger lgr = Logger.getLogger(CopyFrom.class.getName());
+                lgr.log(Level.WARNING, ex.getMessage(), ex);
+            }
+        }
+    }
+    
+    public void All_to_Database(int max){
+        int j = 1;
+        for (int i = 0; i < max; i++){
+            String fileName = "item" + j;
+            File_to_Database(fileName);
+            j++;
+        }
     }
 }
