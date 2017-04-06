e5c52861d21ef4b960e15ce766846178a492b5ca
KrakenSociety <KrakenSociety@users.noreply.github.com>
2017-04-06 12:21:48 -0500

    Update ShoppingCart.java

--- ShoppingCart.java
+++ ShoppingCart.java
@@ -13,18 +13,23 @@ package scamazon;
 import java.util.*;
 public class ShoppingCart {
     
+    //shoppingList is a vector housing the customers items that they are interested in buying
     public Vector<ItemClass> shoppingList = new Vector<ItemClass>(0, 5);
     
+    //totalPrice is the total sum of the item cost in the shoppingList
     public double totalPrice = 0.0;
     
+    //salesTax is the tax that we are going to charge them
     public double salesTax = 0.1;
     
+    //calcTotalPrice is a function that sums up the total price of the items
+    //in the shopping cart
     public double calcTotalPrice(){
         for(int i = 0; i < shoppingList.size(); ++i){
             totalPrice += shoppingList[i].getPrice();
         }
-        salesTax *= totalPrice;
-        totalPrice += salesTax;
+        salesTax *= totalPrice; //calculate sales tax
+        totalPrice += salesTax; //add sales tax to the totalPrice
     }
     
     public void addItem(ItemClass item){
